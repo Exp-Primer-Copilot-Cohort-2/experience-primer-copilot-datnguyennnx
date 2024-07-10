8e15a4953fa0f26c841afff75caeff8df9732425
datnguyennnx <official.nguyendat@gmail.com>
2024-07-10 18:19:17 +0700

    last step

--- comment.js
+++
@@ -1,8 +0,0 @@
-// Create web server
-const express = require("express");
-const app = express();
-const port = 3000;
-app.use(express.static(__dirname + "/public"));
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
